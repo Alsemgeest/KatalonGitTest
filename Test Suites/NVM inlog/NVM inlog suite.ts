138910467755490c3a75521d020e22005f9d5fc8
kal <kal@QD-NB-040.QDelft.Local>
2018-10-22 09:05:31 +0200

    ADD suite collection NVM inlog

---
+++ Test Suites/NVM inlog/NVM inlog suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NVM inlog suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ef9910ad-02b2-4ed2-892f-65d730c84a66</testSuiteGuid>
+   <testCaseLink>
+      <guid>e6096134-d50d-424c-ad68-4417784b182f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NVM inlog LEDEN</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b73156f1-e914-4b4e-bfb8-1df7c73090b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NVM inlog STAMKAART</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
